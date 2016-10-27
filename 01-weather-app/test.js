1825932ee466dc9f8d7f01ffbfd79a65013a355d
yemaocheng <yemaocheng@gmail.com>
2016-10-27 13:38:16 +0800

    'modle-view-binding'

---
+++ 01-weather-app/test.js
@@ -0,0 +1,52 @@
+(function(){
+  /**
+* SETUP
+**/
+var app = app || {};
+
+/**
+* MODELS
+**/
+app.Message = Backbone.Model.extend({  
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=Taipei&APPID=2ab10d1d7c261f5cb373916cc1cf107f',
+  defaults: {
+    main: {
+        temp: -1,
+        humidity: -1
+    },
+    wind: {
+        speed: -1
+    }
+  }
+});
+
+var message = new app.Message();
+
+$(document).ready(function(){
+  var render = function(){
+    // Celsius
+        var temp = message.get('main').temp;
+        var celsius = parseInt(temp - 273.15);
+        message.set('celsius', celsius);
+
+
+        // Date
+        var date = moment().format('LL');
+        message.set('date', date);
+        
+        var html = template(message.attributes);
+       
+       $('#app').html(html);
+
+};
+var template = _.template($("#weather-tmpl").html());
+
+message.fetch({
+  success:function(model, resp, options){
+    console.log(resp);
+    render();
+  }
+});
+
+});
+})();
